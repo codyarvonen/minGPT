3637b71d4cf7cb70e78bc3bce9578edc39125ec2
codyarvonen <codyarvonen@gmail.com>
2023-11-13 23:36:05 -0700

    update run script

--- run_training.sh
+++ run_training.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
+#SBATCH --ntasks=1
 #SBATCH --nodes=1
-#SBATCH --gpus-per-task=8
-#SBATCH --mem=0
+#SBATCH --gpus=1
+#SBATCH --mem-per-cpu=64000M
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END
 #SBATCH --mail-type=FAIL
 #SBATCH --qos=cs
 #SBATCH --partition=cs
-#SBATCH --time=8:00:00
+#SBATCH --time=4:00:00
 module load python/3.11
 source gptenv/bin/activate
 python train.py
